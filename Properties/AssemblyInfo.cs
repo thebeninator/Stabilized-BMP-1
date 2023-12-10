ddf6b082d16821558ddcfd813957cfad5b8cdf02
Benjamin Nguyen <xccr123@gmail.com>
2023-12-09 19:14:23 -0800

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StabilizedBMP1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StabilizedBMP1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b18aacf6-5522-4e9f-b94c-7b51a64f8d5a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StabilizedBMP1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using MelonLoader;
+using StabilizedBMP1;
+using GHPC.Vehicle;
+using GHPC;
+using System.Reflection;
+using GHPC.Weapons;
+
+[assembly: MelonInfo(typeof(StabilizedBMP1Mod), "Stabilized BMP-1", "1.0.0", "ATLAS")]
+[assembly: MelonGame("Radian Simulations LLC", "GHPC")]
+
+
+namespace StabilizedBMP1
+{
+    public class StabilizedBMP1Mod : MelonMod
+    {
+        private GameObject[] vic_gos;
+        private string[] invalid_scenes = new string[] { "MainMenu2_Scene", "LOADER_MENU", "LOADER_INITIAL", "t64_menu" };
+
+        public override async void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            if (invalid_scenes.Contains(sceneName)) return;
+            vic_gos = GameObject.FindGameObjectsWithTag("Vehicle");
+
+            while (vic_gos.Length == 0)
+            {
+                vic_gos = GameObject.FindGameObjectsWithTag("Vehicle");
+                await Task.Delay(1);
+            }
+
+            await Task.Delay(3000);
+
+            foreach (GameObject vic_go in vic_gos)
+            {
+                Vehicle vic = vic_go.GetComponent<Vehicle>();
+
+                if (vic == null) continue;
+
+                string name = vic.FriendlyName;
+
+                if (name == "BMP-1" || name == "BMP-1P")
+                {
+                    AimablePlatform[] aimables = vic.AimablePlatforms;
+                    FieldInfo stab_mode = typeof(AimablePlatform).GetField("_stabMode", BindingFlags.Instance | BindingFlags.NonPublic);
+                    FieldInfo stab_active = typeof(AimablePlatform).GetField("_stabActive", BindingFlags.Instance | BindingFlags.NonPublic);
+                    PropertyInfo stab_FCS_active = typeof(FireControlSystem).GetProperty("StabsActive", BindingFlags.Instance | BindingFlags.Public);
+
+                    WeaponsManager weapons_manager = vic.GetComponent<WeaponsManager>();
+                    WeaponSystemInfo main_gun_info = weapons_manager.Weapons[0];
+                    stab_FCS_active.SetValue(main_gun_info.FCS, true);
+                    main_gun_info.FCS.CurrentStabMode = StabilizationMode.Vector;
+
+                    aimables[0].Stabilized = true;
+                    stab_active.SetValue(aimables[0], true);
+                    stab_mode.SetValue(aimables[0], StabilizationMode.Vector);
+
+                    aimables[3].Stabilized = true;
+                    stab_active.SetValue(aimables[3], true);
+                    stab_mode.SetValue(aimables[3], StabilizationMode.Vector);
+                }
+            }
+        }
+    }
+}
